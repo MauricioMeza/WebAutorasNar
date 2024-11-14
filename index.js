2c03b691e5c5f9a5de694ad70a70d2aa48fd8781
Mauricio Meza <mmezab@unal.edu.co>
2024-11-14 16:59:41 -0500

    auto-scroll

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function openPopup() {
 function closePopup() {
     document.querySelector('.overlay').style.display = 'none';
 }
+
+
+const scrollContainer = document.getElementById("content-time");
+let scrollAmount = 0;
+let scrollDirection = 1;
+const scrollSpeed = 0.4; // Adjust this value to control the scroll speed
+let autoScrolling = true;
+
+// Function to automatically scroll horizontally
+function autoScroll() {
+  if (!autoScrolling) return;
+  if (scrollAmount >= scrollContainer.scrollWidth - scrollContainer.clientWidth || scrollAmount < 0) {
+    scrollDirection *= -1;
+  }
+  scrollAmount += scrollDirection * scrollSpeed;
+  scrollContainer.scrollLeft = Math.floor(scrollAmount);
+  requestAnimationFrame(autoScroll);
+}
+
+scrollContainer.addEventListener('mousedown', () => autoScrolling = false);
+scrollContainer.addEventListener('touchstart', () => autoScrolling = false);
+
+
+
+
+// Start the scrolling
+autoScroll();
+
