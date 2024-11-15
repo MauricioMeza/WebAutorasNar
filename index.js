706267981791fafca7b8aa4f18607c06fea3cbe2
Mauricio Meza <mmezab@unal.edu.co>
2024-11-15 18:14:34 -0500

    avances data y fotos autoras

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 var data = [
-  {},
+  {
+    nombre: "Angela María Alarcón Montenegro ",
+    experiencia: 6,
+    nacimientoLugar: "Pasto",
+    nacimientoFecha: 1992,
+    residencia: "Pasto",
+    areas: "Teatro sala, Teatro experimental, performance.",
+    obras: [
+      {nombre: "Primogénito pecado", año: 2014},
+      {nombre: "Nocturna", año: 2015},
+      {nombre: "Los viajeros", año: 2016},
+      {nombre: "Recuerde el cambio está en sus manos", año: 2016},
+      {nombre: "Dónde no puedo volver", año: 2018},
+      {nombre: "¿Y mis hijos? Trilogía distópica", año: 2017},
+      {nombre: "Otros tantos textos inéditos"},
+    ],
+    otros: [
+      {nombre: "Antología relata ISBN 978-958-8822-73-0", año: 2014},
+      {nombre: "Memo, el elefante itineranteLos viajes de Tespis ISBN 978-958-759-193-4"},
+      {nombre: "Revista de las artes escénicas,Repositorio Universidad Nacional", año: 2015},
+    ],
+    reconocimientos: [
+      {nombre: "Portafolio de estímulos Pasto la gran Capital - Premio a texto de teatro Creación Dramaturgia para  Teatro, Circo y/o Títeres. Con la obra “¿Y mis hijos? Una trilogía distópica", año: 2022}
+    ],
+    perfil: 'Ángela Alarcón, dramaturga, licenciada en Artes Escénicas y magíster en Escrituras Creativas, es una artista que transforma lo cotidiano en historias que capturan la esencia humana. Inspirada en la reflexión de Marco Antonio de la Parra sobre la dramaturga como "voyerista", encuentra en las conversaciones, el azar y los fragmentos de historias su mayor fuente de inspiración. Su trayectoria incluye la escritura escénica, la producción de eventos teatrales y la docencia. Actualmente, explora el Barniz de Pasto - Mopa Mopa, una técnica artesanal, como una nueva forma para expandir su lenguaje artístico. Con la firme convicción de que "toda historia merece ser contada", su obra invita a redescubrir las narrativas de lo cotidiano, revelando la belleza y complejidad de lo humano en cada escena y conectando al público con los pequeños y grandes momentos de la vida.',
+    foto: "./assets/Fotografía Portada - Ángela Alarcón.jpg"
+  },
   {
     nombre: "Silvia Rodríguez Narváez",
     experiencia: 17,
@@ -33,7 +59,27 @@ var data = [
     ],
     perfil: "Licenciada en Filosofía y Letras y artista investigadora de la ciudad de Pasto. Hace 17 años se desenvuelve como actriz dentro de compañías de mediana y larga trayectoria donde, además, ha recibido formación en teatro sala, clown dramático, calle, circo y dirección. Su trayectoria dramatúrgica se ha fortalecido de la mano de maestros como Sergio Blanco, Paco Zarzoso, Verónica Musalem, Juan Pablo Troncoso y Ana Melo. Su investigación se centra en el teatro como medio para la reparación simbólica y la construcción de paz en su país. Con base en esto, ha venido trabajando como lazo social del Centro Nacional de Memoria Histórica apoyando iniciativas en los departamentos de Nariño y Putumayo. \n Esta dramaturga se interesa por los clásicos del teatro, especialmente por su ruptura en el contexto social actual y su vigencia en una realidad latinoamericana. Además, destaca su interés en la teoría teatral como puente para la comprensión del problema escénico en la creación."
   },
-  {},
+  {
+    nombre: "Fernanda Villarreal",
+    experiencia: 6,
+    nacimientoLugar: "Pasto",
+    nacimientoFecha: 1996,
+    residencia: "Pasto",
+    areas: "Performance, teatro calle, teatro sala.",
+    obras: [
+      {nombre: "El símbolo oculto"},
+      {nombre: "Voces bajo el mar violeta - Performance"}
+    ],
+    creacionColectiva: [
+      {nombre: "Los cuentos del espíritu - Adaptación"}
+    ],
+    reconocimientos: [
+      {nombre: 'Portafolio de Estímulos “Gran Capital de la Alcaldía de Pasto", categoría obra de ficción no publicada con Voces bajo el mar violeta', año: 2023}
+    ],
+    perfil: 'Esta dramaturga creció en el barrio El Rosario, en la zona suroriental de la ciudad de Pasto, un área frecuentemente olvidada junto al corregimiento de Jamondino, esta artista ha encontrado en las periferias una fuente profunda de inspiración. Su obra “Voces bajo el mar violeta” surgió de su primera entrevista a vecinas y amigas del barrio, descubriendo en ellas lugares y territorios vivos. Además de su trayectoria como actriz de teatro y escritora de minificción, es una artista de performance cuya práctica explora las fronteras entre el cuerpo, la acción y el espacio, creando experiencias que invitan al espectador a vivir el arte de manera visceral y participativa. Su faceta como docente de lengua castellana y literatura y su labor como co fundadora de Casa Tomada, un espacio cultural en su comunidad, refuerzan su compromiso con el desarrollo y la visibilidad de las expresiones artísticas en su territorio. Su trabajo busca dar voz a lo invisible y a las historias “marginadas”, creando un puente entre el arte y su comunidad. A través de sus diversas disciplinas artísticas, se dedica a conectar lo vivido con lo expresado, transformando lo cotidiano en un acto de resistencia y revelación.',
+    foto: "./assets/Fotografía Portada - Fernanda Enríquez.jpg"
+  }
+,
   {},
   {},
   {},
@@ -46,6 +92,7 @@ const areasRes = document.getElementById("areasA");
 const nombrRes = document.getElementById("nombreA");
 const prfilRes = document.getElementById("perfilA");
 const nacmtRes = document.getElementById("nacimientoA");
+const imgenRes = document.getElementById("popup-img");
 
 function openPopup(index) {
     document.querySelector('.overlay').style.display = 'flex';
@@ -55,12 +102,20 @@ function openPopup(index) {
       areasRes.innerHTML = "Areas - <b>" + truncateString(data[index].areas, 85) + "</b>";
       prfilRes.innerHTML = truncateString(data[index].perfil, 275);
       nacmtRes.innerHTML = "Nacimiento, <b>" + data[index].nacimientoLugar + "</b>, " + data[index].nacimientoFecha;
+      console.log(data[index].foto);
+      if(data[index].foto != undefined){
+        imgenRes.style.backgroundImage = 'url("' + data[index].foto + '")';
+      }else{
+        imgenRes.style.backgroundImage = '';  
+      }
+      
     }catch{
       nombrRes.innerHTML = "Nombre de la Autora";
       exprcRes.innerHTML = "00 Años";
       areasRes.innerHTML = "Area1, Area2, Area3";
       prfilRes.innerHTML = truncateString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 275);
       nacmtRes.innerHTML = "Nacimiento, <b>Ciudad</b>, Año";
+      imgenRes.style.backgroundImage = '';  
     }
     
   }
