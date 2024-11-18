cae4a0ba130e45a629cd2d1239a13f05d6541838
Mauricio Meza <mmezab@unal.edu.co>
2024-11-18 10:30:23 -0500

    avances datos autoras

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ var data = [
     perfil: 'Ángela Alarcón, dramaturga, licenciada en Artes Escénicas y magíster en Escrituras Creativas, es una artista que transforma lo cotidiano en historias que capturan la esencia humana. Inspirada en la reflexión de Marco Antonio de la Parra sobre la dramaturga como "voyerista", encuentra en las conversaciones, el azar y los fragmentos de historias su mayor fuente de inspiración. Su trayectoria incluye la escritura escénica, la producción de eventos teatrales y la docencia. Actualmente, explora el Barniz de Pasto - Mopa Mopa, una técnica artesanal, como una nueva forma para expandir su lenguaje artístico. Con la firme convicción de que "toda historia merece ser contada", su obra invita a redescubrir las narrativas de lo cotidiano, revelando la belleza y complejidad de lo humano en cada escena y conectando al público con los pequeños y grandes momentos de la vida.',
     foto: "./assets/Fotografía Portada - Ángela Alarcón.jpg"
   },
+  
   {
     nombre: "Silvia Rodríguez Narváez",
     experiencia: 17,
@@ -57,8 +58,10 @@ var data = [
       {nombre: "Dramaturgias de Mujeres del Sur con ATARAXIA -  ISBN: 978-958-53721-0-8", año: 2024},
       {nombre: "Panorama teatral colombiano: Dramaturgias emergentes -  ISBN: 978-628-01-5716-0"},
     ],
-    perfil: "Licenciada en Filosofía y Letras y artista investigadora de la ciudad de Pasto. Hace 17 años se desenvuelve como actriz dentro de compañías de mediana y larga trayectoria donde, además, ha recibido formación en teatro sala, clown dramático, calle, circo y dirección. Su trayectoria dramatúrgica se ha fortalecido de la mano de maestros como Sergio Blanco, Paco Zarzoso, Verónica Musalem, Juan Pablo Troncoso y Ana Melo. Su investigación se centra en el teatro como medio para la reparación simbólica y la construcción de paz en su país. Con base en esto, ha venido trabajando como lazo social del Centro Nacional de Memoria Histórica apoyando iniciativas en los departamentos de Nariño y Putumayo. \n Esta dramaturga se interesa por los clásicos del teatro, especialmente por su ruptura en el contexto social actual y su vigencia en una realidad latinoamericana. Además, destaca su interés en la teoría teatral como puente para la comprensión del problema escénico en la creación."
+    perfil: "Licenciada en Filosofía y Letras y artista investigadora de la ciudad de Pasto. Hace 17 años se desenvuelve como actriz dentro de compañías de mediana y larga trayectoria donde, además, ha recibido formación en teatro sala, clown dramático, calle, circo y dirección. Su trayectoria dramatúrgica se ha fortalecido de la mano de maestros como Sergio Blanco, Paco Zarzoso, Verónica Musalem, Juan Pablo Troncoso y Ana Melo. Su investigación se centra en el teatro como medio para la reparación simbólica y la construcción de paz en su país. Con base en esto, ha venido trabajando como lazo social del Centro Nacional de Memoria Histórica apoyando iniciativas en los departamentos de Nariño y Putumayo. \n Esta dramaturga se interesa por los clásicos del teatro, especialmente por su ruptura en el contexto social actual y su vigencia en una realidad latinoamericana. Además, destaca su interés en la teoría teatral como puente para la comprensión del problema escénico en la creación.",
+    foto: "./assets/Fotografía Portada - Silvia.jpeg"
   },
+  
   {
     nombre: "Fernanda Villarreal",
     experiencia: 6,
@@ -78,11 +81,70 @@ var data = [
     ],
     perfil: 'Esta dramaturga creció en el barrio El Rosario, en la zona suroriental de la ciudad de Pasto, un área frecuentemente olvidada junto al corregimiento de Jamondino, esta artista ha encontrado en las periferias una fuente profunda de inspiración. Su obra “Voces bajo el mar violeta” surgió de su primera entrevista a vecinas y amigas del barrio, descubriendo en ellas lugares y territorios vivos. Además de su trayectoria como actriz de teatro y escritora de minificción, es una artista de performance cuya práctica explora las fronteras entre el cuerpo, la acción y el espacio, creando experiencias que invitan al espectador a vivir el arte de manera visceral y participativa. Su faceta como docente de lengua castellana y literatura y su labor como co fundadora de Casa Tomada, un espacio cultural en su comunidad, refuerzan su compromiso con el desarrollo y la visibilidad de las expresiones artísticas en su territorio. Su trabajo busca dar voz a lo invisible y a las historias “marginadas”, creando un puente entre el arte y su comunidad. A través de sus diversas disciplinas artísticas, se dedica a conectar lo vivido con lo expresado, transformando lo cotidiano en un acto de resistencia y revelación.',
     foto: "./assets/Fotografía Portada - Fernanda Enríquez.jpg"
+  },
+  
+  {
+    nombre: "La Oreja que Habla.",
+    experiencia: 12,
+    nacimientoLugar: "Pasto",
+    nacimientoFecha: 1996,
+    residencia: "Cali",
+    areas: "Performance, teatro calle, teatro sala.",
+    obras: [
+      {nombre: "El símbolo oculto"},
+      {nombre: "Voces bajo el mar violeta - Performance"}
+    ],
+    creacionColectiva: [
+      {nombre: "Los cuentos del espíritu - Adaptación"}
+    ],
+    reconocimientos: [
+      {nombre: 'Portafolio de Estímulos “Gran Capital de la Alcaldía de Pasto", categoría obra de ficción no publicada con Voces bajo el mar violeta', año: 2023}
+    ],
+    perfil: 'Esta dramaturga creció en el barrio El Rosario, en la zona suroriental de la ciudad de Pasto, un área frecuentemente olvidada junto al corregimiento de Jamondino, esta artista ha encontrado en las periferias una fuente profunda de inspiración. Su obra “Voces bajo el mar violeta” surgió de su primera entrevista a vecinas y amigas del barrio, descubriendo en ellas lugares y territorios vivos. Además de su trayectoria como actriz de teatro y escritora de minificción, es una artista de performance cuya práctica explora las fronteras entre el cuerpo, la acción y el espacio, creando experiencias que invitan al espectador a vivir el arte de manera visceral y participativa. Su faceta como docente de lengua castellana y literatura y su labor como co fundadora de Casa Tomada, un espacio cultural en su comunidad, refuerzan su compromiso con el desarrollo y la visibilidad de las expresiones artísticas en su territorio. Su trabajo busca dar voz a lo invisible y a las historias “marginadas”, creando un puente entre el arte y su comunidad. A través de sus diversas disciplinas artísticas, se dedica a conectar lo vivido con lo expresado, transformando lo cotidiano en un acto de resistencia y revelación.',
+    foto: "./assets/Fotografía Portada - La oreja que habla.jpg"
+  },
+
+  {nombre: "Brigitte Georgina Andrade Erazo",
+    experiencia: 3,
+    nacimientoLugar: "Pasto",
+    nacimientoFecha: 1996,
+    residencia: "Pasto",
+    areas: "Teatro circo, teatro títeres u objetos, teatro comunitario, performance.",
+    obras: [
+      {nombre: "Awawiti guardiana del agua", año: 2023},
+    ],
+    reconocimientos: [
+      {nombre: 'Portafolio de Estímulos “Gran Capital de la Alcaldía de Pasto", categoría circulación de circo con “Awawiti guardiana del agua”.', año: 2022}
+    ],
+    perfil: 'Briggitte Andrade es una creadora apasionada por el clown, las artes escénicas y el trabajo comunitario. Madre y gestora cultural, su compromiso con la infancia y su territorio la ha llevado a organizar encuentros culturales y proyectos que fortalecen el tejido social. En 2023, dio un paso decisivo en su carrera al crear su primera obra como dramaturga, consolidando su voz artística. Enamorada del campo y caminante de la ciudad, su trayectoria refleja una profunda conexión con su entorno y una lucha constante por crecer como artista en contextos adversos. Actualmente, se enfoca en su desarrollo profesional y en promover la participación y el aprendizaje en su comunidad, especialmente entre los más jóvenes.',
+    foto: "./assets/Fotografía Portada - Briggite.jpeg"
+  },
+
+  {
+    nombre: "Angie Carolina Rodríguez Basante ",
+    experiencia: 13,
+    nacimientoLugar: "Pasto",
+    nacimientoFecha: 1997,
+    residencia: "Pasto",
+    areas: "Teatro sala, circo, teatro títeres y objetos.",
+    obras: [
+      {nombre: "Nos siguen pegando abajo", año: 2022},
+      {nombre: "Avenida sur", año: 2023},
+    ],
+    creacionColectiva: [
+      {nombre: "Nada pasa", año: 2021},
+      {nombre: "Qué ruido hace el silencio", año: 2022},
+      {nombre: "Memo, el elefante itinerante", año: 2023},
+    ],
+    reconocimientos: [
+      {nombre: 'Convocatoria Pública Departamental de Estímulos “Mi Nariño Cultura Viva”. Con “Qué ruido hace el silencio”', año: 2023},
+      {nombre: 'Reconocimiento por  la obra de teatro “Qué ruido hace el silencio” otorgado por Fundación Antropomorfa”', año: 2022},
+      
+    ],
+    perfil: "Artista escénica, diseñadora industrial y artesana de Pasto, con 13 años de experiencia en teatro, telas acrobáticas y acrobacia en dúo, ha construido una trayectoria que la ha llevado a festivales locales, nacionales e internacionales. Además, cuenta con un diplomado en la técnica artesanal del Barniz de Pasto - Mopa Mopa, que integra como parte de su expresión artística. La obra de esta dramaturga es una búsqueda constante por trascender lo real y explorar “no lugares” que florecen fuera de los espacios convencionales. Inspirada por imágenes de agua y cielo, su escritura y escenografía revelan mundos invisibles, metáforas hechas tangibles, donde se manifiestan imposibilidades y ensueños. Su teatro invita a ver el mundo con otros ojos, a encontrar realidades alternas y explorar lo desconocido. Su estilo evoca sonidos urbanos y elementos del rock latinoamericano, combinando la intensidad de las guitarras y el espíritu de la ciudad en una propuesta que desafía los límites de la realidad y la imaginación.",
+    foto: "./assets/Fotografía Portada - Carolina Rodriguez.jpg"
   }
 ,
-  {},
-  {},
-  {},
 ]
 
 const scrollContainer = document.getElementById("content-time");
